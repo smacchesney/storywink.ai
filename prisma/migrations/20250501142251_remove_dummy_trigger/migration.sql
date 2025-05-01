d840bd6853ecb4525289a19b313aba9541312b73
Your Name <you@example.com>
2025-05-02 00:33:18 +0800

    finalize-checker: add dockerfile

--- prisma/migrations/20250501142251_remove_dummy_trigger/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `___trigger` on the `UserProfile` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "UserProfile" DROP COLUMN "___trigger";
